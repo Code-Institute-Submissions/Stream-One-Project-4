a5890bc1aa95f0ff9f5d9da55bca9036fcb9c596
Colm Hughes <cjhugo1991@hotmail.com>
2018-05-13 20:10:49 +0000

    Frank and Wireframes added

--- README.md
+++ README.md
@@ -1,14 +1,62 @@
 # It's Always Sunny In Philadelphia Website
+This is solely a front-end project, built using HTML, CSS and JavaScript.
+It was built as one of three projects for my Code Institute bootcamp course.
 
-This website has a page dedicated to each member of 'the gang'.
-
-
-## What does it do?
-
-Each page is designed from the charcters view and intends to show them in the 
+It's Always Sunny In Philadelphia is a comedy tv series that has been running since 2005.
+It centres around the owners of the fictional Paddy's Pub in Philadelphia. Charlie, Dennis, Mac, Dee and Frank who are
+collectively known as 'the gang' spend their days making elaborate plans and conspiring against one another.
+This website has a page dedicated to each member of 'the gang' who give a brief insight into their minds.
+ 
+Each page is designed from my opinion of the charcters view and intends to show them in their
 best light.
 
-### How does it work
- 
-The site is styled with **Bootstrap**. The quiz has been created using **Javascript** 
-and **Jquery**. The site can be viewed [HERE](https://colmhughes.github.io/Stream-One-Project/)
+## Demo
+The deployed version of the web app is available on github pages: https://colmhughes.github.io/Stream-One-Project/
+
+## View locally
+The repository is available on github: https://github.com/ColmHughes/grandslams-project2.
+The git repository can be cloned and then simply run the index.html file.
+
+## Built with 
+1. HTML
+2. CSS
+3. Bootstrap
+4. JavaScript
+5. JQuery
+
+
+## Media
+Images were sourced from google images, the youtube video was embedded using the iframe
+html tag and audio was downloaded from zapsplat.com. The text contained on this site is
+a blend of paraphrasing and direct quotes from the show.
+
+
+## Wireframes
+The wireframes for this project can be viewed on github in the 'Wireframes' folder. The project is
+fully mobile-responsive with the only major difference being the home page and navbar which have separate sketches
+for mobile and desktop. All sketches were made on 'wireframepro'.
+
+## Testing
+The project was tested on many different devices and across a range of browsers.
+No automated testing was used, however all javascript/jquery was manually tested.
+
+Quiz: 
+Inside the form I included type="number" into the input tag to limit the user to submitting numbers, I also
+included the keyword required.
+
+Each answer was parsed into an integer and added together to get our total. Our total determines which image is shown.
+Each answer was also passed into a function to ensure the number is between one and five inclusive, numbers outside this
+range result in a modal advising the user to input a correct number.
+
+The booking form, blackboard writing and carousel were also tested through a process of trial and error until the
+desired effect was achieved.
+
+
+## Left To Do
+I intend on reworking this project so it uses angularjs, in order to gain a better understanding of the technology and
+as a comparative test to see the pros and cons of each.
+
+
+
+
+
--- script.js
+++ script.js
@@ -3,35 +3,54 @@ $(document).ready(function(){
     
     
 // -------------------- Charlie --------------------
+
   let answerform = document.getElementById("myForm");
   $(".work").click(function() {
         let total = parseInt(answerform["answer1"].value) + parseInt(answerform["answer2"].value)
         + parseInt(answerform["answer3"].value) + parseInt(answerform["answer4"].value);
-        if (total<= 4) {
-            
-            // document.getElementById('myImage').src='images/Charlie5.jpg';
-            $("#myImage").attr("src", "images/Charlie10.jpg");
-        } else if (total <= 8) { 
-            $("#myImage").attr("src", "images/Charlie10.jpg");
-          
-        } else if (total <= 12){
-            $("#myImage").attr("src", "images/Charlie15.jpg");
-        } else if (total <= 16){
-            $("#myImage").attr("src", "images/Charlie19.jpg"); 
-        } else {
-            $("#myImage").attr("src", "images/Charlie20.jpg");
-        }
-    // $(".whatCharlie").fadeIn(4000);
-    $(".whatWeapon").fadeIn(8000);
         
-    $(".formDiv").remove();
+        function withinRange(question) {
+          let letter = parseInt(answerform[question].value);
+          if (letter < 0 || letter > 5 || letter === "") {
+              console.log(letter)
+              $("#error").modal();
+              return false;
+          } else {
+              return letter;
+          }
+        }
+        let w = withinRange("answer1");
+        let x = withinRange("answer2");
+        let y = withinRange("answer3");
+        let z = withinRange("answer4");
+        
+        console.log(w)
+        console.log(x)
+        console.log(y)
+        console.log(z)
+        
+        
+        if (w != false && x != false && y != false && z != false && w > 0 && x > 0 && y > 0 && z > 0){
+          if (total<= 4) {
+              $("#myImage").attr("src", "images/Charlie10.jpg");
+          } else if (total <= 8) { 
+              $("#myImage").attr("src", "images/Charlie10.jpg");
+          } else if (total <= 12){
+              $("#myImage").attr("src", "images/Charlie15.jpg");
+          } else if (total <= 19){
+              $("#myImage").attr("src", "images/Charlie19.jpg"); 
+          } else {
+              $("#myImage").attr("src", "images/Charlie20.jpg");
+          }
+        
+          $(".details").fadeIn(8000);
+          $(".formDiv").remove();
+        }
     });
   
     $('.tryAgain').click(function() {
         location.reload();
     });
- 
- 
  
  
  
@@ -52,125 +71,37 @@ function typeWriter(text, n, a) {
 typeWriter("The D.E.N.N.I.S System", 0, '.test');
 
 $('.start').click(function() {
-  
   let text = $('.d').data('text');
-  
   typeWriter(text, 0, '.d');
-  
-});  
+}); 
+
 $('.start2').click(function() {
-  
   let text = $('.e').data('text');
   typeWriter(text, 0, '.e');
-  
 });
+
 $('.start3').click(function() {
-  
   let text = $('.n1').data('text');
   typeWriter(text, 0, '.n1');
-  
 });
+
 $('.start4').click(function() {
-  
   let text = $('.n2').data('text');
   typeWriter(text, 0, '.n2');
-  
 });
+
 $('.start5').click(function() {
-  
   let text = $('.i').data('text');
   typeWriter(text, 0, '.i');
-  
 });
+
 $('.start6').click(function() {
-  
   let text = $('.s').data('text');
   typeWriter(text, 0, '.s');
 });
 
 
-
-
-
-
-
- 
-    // $(".dennis").mouseenter(function() {
-    // 	$(this).css("color", "red");
-    // 	$(this).children("span").css("color", "red");
-    // 	$(this).children("span").show();
-    // });
- 
-    // $(".dennis").mouseleave(function() {
-	   //$(this).siblings().fadeTo(500,1);
-	   //$(this).css("color", "black");
-	   //$(this > "span").css("color", "black");
-	   //$("span").hide();
-    // // });
-    // $(".d").click(function() {
-    //     $(".tL").html("<h2>Demonstrate Value</h2>");
-    // //     (".tL").addClass('topRight');
-    // //     // $(".top.left").addClass('show');
-    // //     $(this).css("color", "red");
-    // // 	$(this).children("span").css("color", "red");
-    // // 	$(this).children("span").show();
-    // // // 	$(".topLeft").fadeTo(3000,1)
-    // });
-    // $(".tL").click(function() {
-    //     $(".d").html("<p>Demonstrate Value</p>");
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-    // });
-    // $(".tR").click(function() {
-    //     $(".e").html("<p>Engage Physically</p>");
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-    // });
-    //  $(".mL").click(function() {
-    //     $(".n1").html("<p>Nurture Dependence</p>");
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-    // });
-    //  $(".mR").click(function() {
-    //     $(".n2").html("<p>Neglect Emotionally</p>");
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-    // });
-    //  $(".bL").click(function() {
-    //     $(".i").html("<p>Inspire Hope</p>");
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-    // });
-    //  $(".bR").click(function() {
-    //     $(".s").html("<p>Separate Entirely</p>");
-    //     $(".header").fadeIn(2000);
-    //     $(".dennisHeader").fadeIn(2000);
-    //     $(".dennisPara").fadeIn(2000);
-    //     $(this).children("button").css({"background-color": "#176905", "border-color": "#48A136"});
-        
-    // });
-    
-    
-    
-
 //  -------------------- Dee --------------------
-// $('#myCarousel').carousel({
-//   interval: 10000
-// })
-
-
-
-//   $(".carousel .item").each(function(){
-//   let next = $(this).next();
-//   if (!next.length) {
-//     next = $(this).siblings(":first");
-//   }
-//   next.children(":first-child").clone().appendTo($(this));
-  
-//   if (next.next().length>0) {
-//     next.next().children(":first-child").clone().appendTo($(this));
-//   }
-//   else {
-//   	$(this).siblings(":first").children(":first-child").clone().appendTo($(this));
-//   }
-// });
- 
-
 
  $('.multi-item-carousel .item').each(function(){
 // check the screen width
@@ -192,5 +123,13 @@ let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
           } 
     }   
 });
+
+$("#UserRegForm").on('submit', function(e) {
+  console.log("Whatup")
+  $("#validate").modal();
+  e.preventDefault();
+});
+
+
 
 });
